b141f5cd9017d52c065de550a6fdab98623c740b
Nathan Jolivet <nathan.jolivet@gmail.com>
2021-08-24 16:30:09 +0200

    update

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,7 +1,5 @@
 class Friendship < ApplicationRecord
-  belongs_to :requester
-  belongs_to :addressed
-  belongs_to :requester_id, class_name: 'User'
-  belongs_to :addressed_id, class_name: 'User'
+  belongs_to :requester, class_name: 'User'
+  belongs_to :addressed, class_name: 'User'
 
 end
