3278e490c3a832d954ca1bdaf2b6bdfe119500ad
Nathan Jolivet <nathan.jolivet@gmail.com>
2021-08-25 12:10:48 +0200

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :photo
   has_many :requester_friendships, class_name: 'Friendship', foreign_key: 'requester_id'
   has_many :addressed_friendships, class_name: 'Friendship', foreign_key: 'addressed_id'
   devise :database_authenticatable, :registerable,
