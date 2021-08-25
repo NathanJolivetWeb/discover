979a8f18759e7cc6153aa40bd00e774f8ac9196f
Rzt Prog <rztprog@outlook.fr>
2021-08-25 17:56:05 +0200

    Create profile show(view) and security to controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,26 @@
 class ProfilesController < ApplicationController
+  before_action :user_id, only: %i[show edit update]
+  def show
+    if (@user != current_user)
+      redirect_to root_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if (@user == current_user)
+      @user.update(user_params)
+    end
+  end
+
+  private
+  def user_id
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :password, :email)
+  end
 end
