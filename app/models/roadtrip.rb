8492799cb379cb300c289d3745560ce3db2ac94a
Nathan Jolivet <nathan.jolivet@gmail.com>
2021-08-25 14:34:11 +0200

    WIP

--- app/models/roadtrip.rb
+++ app/models/roadtrip.rb
@@ -1,6 +1,7 @@
 class Roadtrip < ApplicationRecord
   has_many :itineraries
   has_many :messages
-  has_many :partners
+  has_many :partners, dependent: :destroy
   has_many :tasks
+  has_many :users, through: :partners
 end
