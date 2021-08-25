f900edf88b2016381a13cfc39f54ea589c6b92ff
Nathan Jolivet <nathan.jolivet@gmail.com>
2021-08-25 18:02:50 +0200

    wip

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,5 +1,5 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navbar-discover');
+  const navbar = document.querySelector('.transparent');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight /2) {
