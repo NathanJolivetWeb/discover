d214ac07d7c2a58a3209eef07b5d0ee97b59f803
Rzt Prog <rztprog@outlook.fr>
2021-08-26 15:49:05 +0200

    Wip checklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,37 @@
 class TasksController < ApplicationController
+  before_action :params_id, only: %i[show edit update destroy]
+  before_action :roadtrip_id, only: %i[new create]
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    raise
+    @task = Task.new(task_params)
+    @task.roadtrip = @roadtrip
+
+    if @task.save
+      redirect_to roadtrip_path(@roadtrip.id)
+    end
+
+    redirect_to roadtrip_path(@roadtrip.id)
+  end
+
   def index
+  end
+
+  private
+
+  def roadtrip_id
+    @roadtrip = Roadtrip.find(params[:roadtrip_id])
+  end
+
+  def params_id
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(content: "")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :roadtrips do
-    resources :tasks, only: :create
+    resources :tasks, only: %i[new create]
     resources :messages, only: %i[index create]
     resources :itineraries, only: :create
     resources :partners, only: :create
