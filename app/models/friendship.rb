a26bf7c06693d0fed2bc687207f8e8d41a6d3ad6
Christophe Becquey <christophe.becquey@laposte.net>
2021-08-26 23:13:14 +0200

    seeds friendship + un index rapide

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,4 @@
 class Friendship < ApplicationRecord
-  belongs_to :requester, class_name: 'User'
-  belongs_to :addressed, class_name: 'User'
+  belongs_to :requester, class_name: 'User', dependent: :destroy
+  belongs_to :addressed, class_name: 'User', dependent: :destroy
 end
