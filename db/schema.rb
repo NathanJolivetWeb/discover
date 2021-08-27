d90c4380142b524bc3648923cba56b6d861ad2f8
Nathan Jolivet <nathan.jolivet@gmail.com>
2021-08-27 12:28:51 +0200

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_095408) do
+ActiveRecord::Schema.define(version: 2021_08_27_101610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,11 +90,13 @@ ActiveRecord::Schema.define(version: 2021_08_27_095408) do
   end
 
   create_table "steps", force: :cascade do |t|
-    t.integer "order"
+    t.integer "order", default: 0
     t.string "address"
     t.bigint "itinerary_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["itinerary_id"], name: "index_steps_on_itinerary_id"
   end
 
