b403a81e93ae610ffe8f8f0fab2ea088147e531f
Rzt Prog <rztprog@outlook.fr>
2021-08-27 11:39:42 +0200

    Commit for switch branch

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,33 @@
 class TasksController < ApplicationController
-  def index
+  before_action :params_id, only: :destroy
+  before_action :roadtrip_id, only: %i[create]
+
+  def create
+    @task = Task.new(task_params)
+    @task.roadtrip_id = @roadtrip.id
+
+    if @task.save
+      redirect_to roadtrip_path(@roadtrip)
+    else
+      redirect_to roadtrip_path(@roadtrip)
+    end
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  private
+
+  def roadtrip_id
+    @roadtrip = Roadtrip.find(params[:roadtrip_id])
+  end
+
+  def params_id
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
