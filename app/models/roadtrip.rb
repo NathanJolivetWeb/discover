251b70a9a1d7f8c39c2f5dab7e5494e162b90763
Christophe Becquey <christophe.becquey@laposte.net>
2021-08-28 15:16:22 +0200

    Pull et merge du master

--- app/models/roadtrip.rb
+++ app/models/roadtrip.rb
@@ -1,6 +1,6 @@
 class Roadtrip < ApplicationRecord
   has_one_attached :photo
-  has_many :itineraries
+  has_many :itineraries, dependent: :destroy
   has_many :messages
   has_many :partners, dependent: :destroy
   has_many :tasks, dependent: :destroy
