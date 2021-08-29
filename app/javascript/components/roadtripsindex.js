826ec2138bdfa6c13c02df86150e8dc236d735c2
Christophe Becquey <christophe.becquey@laposte.net>
2021-08-29 21:55:53 +0200

    Filtres qui marchent

--- app/javascript/components/roadtripsindex.js
+++ app/javascript/components/roadtripsindex.js
@@ -1,24 +1,29 @@
+
 const filterStatus = () => {
- var btnPending = document.querySelector('.btn-pending');
- var btnDone = document.querySelector('.btn-done');
- var btnAll = document.querySelector('.btn-all');
- var pending = document.querySelector('.pending');
- var done = document.querySelector('.finished');
- var card = document.querySelector('.card-product');
+  const card = document.getElementById('card-roadtrip');
+  if(card) {
+    const btnPending = document.querySelector('.btn-pending');
+    const btnDone = document.querySelector('.btn-done');
+    const btnAll = document.querySelector('.btn-all');
+    const pending = document.querySelector('.pending');
+    const finished = document.querySelector('.finished');
+    const card = document.querySelector('.card-product');
 
- btnPending.addEventListener('click', () => {
-  card.style.display = 'none';
-  pending.style.display = 'flex';
- });
+    btnDone.addEventListener('click', () => {
+       card.style.display = 'flex';
+       pending.style.display = 'none';
+     });
 
- btnDone.addEventListener('click', () => {
-  card.style.display = 'none';
-  done.style.display = 'flex';
- });
+    btnPending.addEventListener('click', () => {
+     card.style.display = 'none';
+     pending.style.display = 'flex';
+    });
 
- btnAll.addEventListener('click', () => {
-  card.style.display = 'flex';
- });
+    btnAll.addEventListener('click', () => {
+     pending.style.display = 'flex';
+     finished.style.display = 'flex';
+    });
+  };
 }
 
-export { filterStatus };
+export { filterStatus }
