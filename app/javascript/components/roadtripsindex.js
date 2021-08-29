2b1e8f51b9dba63150c28c17fbc0b3aa1009e2e3
Christophe Becquey <christophe.becquey@laposte.net>
2021-08-29 22:24:49 +0200

    WIP filtres de nouveau + correction indentation

--- app/javascript/components/roadtripsindex.js
+++ app/javascript/components/roadtripsindex.js
@@ -1,29 +1,27 @@
-
 const filterStatus = () => {
-  const card = document.getElementById('card-roadtrip');
-  if(card) {
+  const map = document.getElementById('card-roadtrip')
+  if (map){
     const btnPending = document.querySelector('.btn-pending');
     const btnDone = document.querySelector('.btn-done');
     const btnAll = document.querySelector('.btn-all');
     const pending = document.querySelector('.pending');
-    const finished = document.querySelector('.finished');
+    const done = document.querySelector('.finished');
     const card = document.querySelector('.card-product');
-
-    btnDone.addEventListener('click', () => {
-       card.style.display = 'flex';
-       pending.style.display = 'none';
-     });
 
     btnPending.addEventListener('click', () => {
      card.style.display = 'none';
      pending.style.display = 'flex';
     });
 
-    btnAll.addEventListener('click', () => {
-     pending.style.display = 'flex';
-     finished.style.display = 'flex';
+    btnDone.addEventListener('click', () => {
+     card.style.display = 'flex';
+     pending.style.display = 'none';
     });
-  };
-}
+
+    btnAll.addEventListener('click', () => {
+     card.style.display = 'flex';
+    });
+   };
+  }
 
 export { filterStatus }
