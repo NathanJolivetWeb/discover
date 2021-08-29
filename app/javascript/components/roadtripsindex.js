bb9722647bfb0d2bb329d3d6e181ba5908a75b32
Christophe Becquey <christophe.becquey@laposte.net>
2021-08-29 21:28:06 +0200

    WIP filtre

--- app/javascript/components/roadtripsindex.js
+++ app/javascript/components/roadtripsindex.js
@@ -1,10 +1,24 @@
-const myFunction = () => {
-  var x = document.querySelector("pending");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+const filterStatus = () => {
+ var btnPending = document.querySelector('.btn-pending');
+ var btnDone = document.querySelector('.btn-done');
+ var btnAll = document.querySelector('.btn-all');
+ var pending = document.querySelector('.pending');
+ var done = document.querySelector('.finished');
+ var card = document.querySelector('.card-product');
+
+ btnPending.addEventListener('click', () => {
+  card.style.display = 'none';
+  pending.style.display = 'flex';
+ });
+
+ btnDone.addEventListener('click', () => {
+  card.style.display = 'none';
+  done.style.display = 'flex';
+ });
+
+ btnAll.addEventListener('click', () => {
+  card.style.display = 'flex';
+ });
 }
 
-export { myFunction };
+export { filterStatus };
