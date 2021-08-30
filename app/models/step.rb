5bc8208c8632e710a9168eae0603971fd9beda65
Rzt Prog <rztprog@outlook.fr>
2021-08-30 12:57:10 +0200

    Fix user sign in, and add default image

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,5 +1,5 @@
 class Step < ApplicationRecord
-  has_many :itinerary_steps
+  has_many :itinerary_steps, dependent: :destroy
   has_many :itineraries, through: :itinerary_steps, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
